d6758200a7ee7b6d4d4c606356a34f476c4707ee
Hemant Bhoi <56757949+hkbcode@users.noreply.github.com>
2021-03-13 20:58:26 +0530

    :zap: initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+## Gatsbyjs starter with Chakra UI
+
+
+Gatsbyjs blog with NetlifyCMS and ChakraUI
---
+++ src/pages/about.js
@@ -0,0 +1,19 @@
+import React from "react"
+import { Box, Text } from "@chakra-ui/react"
+
+const AboutPage = () => {
+    return (
+        <Box p={8}>
+            <Text fontSize="xl"> About me Page</Text>
+            <Text fontSize="md">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus, quas ex totam in harum delectus voluptatum repellendus error, vel, at reprehenderit pariatur! Molestiae iusto quia voluptate, molestias magni praesentium est!
+            </Text>
+        </Box>
+
+    );
+}
+
+
+
+
+export default AboutPage
